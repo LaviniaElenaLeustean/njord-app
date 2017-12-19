5a2660591f741ac22032b5f9e509c8898f5a8140
LaviniaElenaLeustean <lavinia.leustean@gmail.com>
2017-12-19 16:24:09 +0200

    linked db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //database:'mongodb://lavi:lavi@ds155644.mlab.com:55644/meanauthappp'
-	database: 'mongodb://localhost:27017/meanauth',
+  database:'mongodb://lavi:lavi@ds155644.mlab.com:55644/meanauthappp'
+//database: 'mongodb://localhost:27017/meanauth',
   secret: 'yoursecret'
 }
